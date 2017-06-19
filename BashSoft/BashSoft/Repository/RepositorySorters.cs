15f5f4cc4ceccee5ea0e9f278799b33ad7a64f97
MiroslavCharakchiev <fearfax2@yahoo.com>
2017-06-19 11:02:18 +0300

    Finish the StoryMode. the Bash is ready.

---
+++ BashSoft/BashSoft/Repository/RepositorySorters.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BashSoft
+{
+    class RepositorySorters
+    {
+        public static void OrderAndTake(Dictionary<string, List<int>> data, string sorter, int StudentsToTake)
+        {
+            sorter = sorter.ToLower();
+            if (sorter == "ascending")
+            {
+                PrintStudents(data.OrderBy(x => x.Value.Sum())
+                    .Take(StudentsToTake)
+                    .ToDictionary(pair => pair.Key, pair => pair.Value));
+            }
+            else if (sorter == "desending")
+            {
+                PrintStudents(data.OrderByDescending(x => x.Value.Sum())
+                    .Take(StudentsToTake)
+                    .ToDictionary(pair => pair.Key, pair => pair.Value));
+            }
+            else
+            {
+                OutputWriter.WriteMessageOnNewLine(ExceptionMessages.InvalidComparisonQuery);
+            }
+        }
+
+        private static void PrintStudents(Dictionary<string, List<int>> studentsSorted)
+        {
+            foreach (var kvp in studentsSorted)
+            {
+                OutputWriter.PrintStudent(kvp);
+            }
+        }
+    }
+}
